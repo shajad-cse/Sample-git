2fb73b0b3d317427bdc76003863fb042c1590b5b
Shajad <shajad.shaik@co>
2019-06-16 23:34:01 +0530

    adding extra lines

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         //TODO THE CODE
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); //Commentd
 
+
+    }
+
+    private static void m1() {
 
     }
 }
